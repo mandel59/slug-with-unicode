927968ceccc7556c4d124342d596196eb328c3a9
Ryusei Yamaguchi <mandel59@gmail.com>
2023-04-23 05:35:26 +0900

    Add reproduction code

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
+import Link from "next/link";
+
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { q?: string | string[] };
+}) {
+  return (
+    <div>
+      <div>q={searchParams.q}</div>
+      <div>
+        <Link href="/another">Another</Link>
+      </div>
+    </div>
+  );
 }
